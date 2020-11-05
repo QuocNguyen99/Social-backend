0dfceeaeac1ba0bfb2c1d4717b1007a81dafb2f0
Nguyên <huynhquocnguyen99@gmail.com>
2020-11-06 01:35:36 +0700

    structure project

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const array = [
+    { id: 1, name: 'Nguyên' },
+    { id: 2, name: 'Tú' },
+    { id: 3, name: 'Huy' }
+]
+
 app.get('/', (req, res) => {
     res.send('Hello world');
+})
+
+app.get('/user', (req, res) => {
+    res.send(array);
 })
 
 const port = process.env.PORT || 3000
