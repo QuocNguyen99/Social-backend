283bd24ff996e8214fc380c18d45c2131a81a880
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2020-11-21 03:21:24 +0700

    123

--- controller/post.js
+++ controller/post.js
@@ -44,7 +44,7 @@ exports.createPost = async (req, res) => {
 
     let post = new Post({
         content: req.body.content,
-        image: [1, 2, 3],
+        image: ['1', '2'],
         author: req.body.author,
         comment: req.body.comment,
         likeUser: req.body.likeUser
