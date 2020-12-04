e14783edfe27bc6b92ccae27582aac9f8631ffff
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2020-12-04 15:09:51 +0700

    comment

--- controller/comment.js
+++ controller/comment.js
@@ -2,8 +2,8 @@ const { validate, Comment } = require("../models/comment");
 const { Post } = require("../models/post");
 
 exports.getListComment = async (req, res) => {
-    const post = await Post.findById(req.body.id);
-    if (!post) return res.status(404).send({ error: true });
+    // const post = await Post.findById(req.body.id);
+    // if (!post) return res.status(404).send({ error: true });
 
     const comments = await Comment.find({ idPost: req.body.id })
         .populate('author', ' displayName image')
